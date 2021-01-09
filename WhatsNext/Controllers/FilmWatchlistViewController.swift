41016e83967fe84f84950d78adcdda8e9327f5f7
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2021-01-09 12:42:42 +0100

    Add searchbar

--- WhatsNext/Controllers/FilmWatchlistViewController.swift
+++ WhatsNext/Controllers/FilmWatchlistViewController.swift
@@ -11,9 +11,11 @@ class FilmCell: UITableViewCell {
     @IBOutlet weak var posterImg: UIImageView!
     @IBOutlet weak var titleLbl: UILabel!
 }
-class FilmWatchlistViewController: UITableViewController {
+class FilmWatchlistViewController: UITableViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     var films: [Film] = []
+    var filteredFilms: [Film]!
     var currentFilm: Film?
     let loadingIndicator = UIActivityIndicatorView(style: .large)
 
@@ -21,31 +23,48 @@ class FilmWatchlistViewController: UITableViewController {
         super.viewDidLoad()
         loadingIndicator.center = view.center
         view.addSubview(loadingIndicator)
+        searchBar.delegate = self
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let filmDetailViewController = segue.destination as! FilmDetailViewController
         let indexPath = tableView.indexPathForSelectedRow
-        filmDetailViewController.film = self.films[indexPath!.row]
+        filmDetailViewController.film = self.filteredFilms[indexPath!.row]
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.fetchFilms()
+        filteredFilms = films
+
     }
 
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+
+            filteredFilms = films
+            tableView.reloadData()
+            return
+        }
+      filteredFilms = films.filter { (film: Film) -> Bool in
+        return film.titel.lowercased().contains(searchText.lowercased())
+      }
+
+      tableView.reloadData()
+    }
     func fetchFilms() {
         loadingIndicator.startAnimating()
         Network.shared.getWatchlist { [self] films in
             self.films = films
+            self.filteredFilms = films
             loadingIndicator.stopAnimating()
             tableView.reloadData()
-            print(self.films.count)
         }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "FilmCell", for: indexPath) as! FilmCell
 
-        let imageURL = URL(string: films[indexPath.row].titleImage)!
+        let imageURL = URL(string: filteredFilms[indexPath.row].titleImage)!
         DispatchQueue.global().async {
             guard let imageData = try? Data(contentsOf: imageURL) else { return }
 
@@ -54,8 +73,7 @@ class FilmWatchlistViewController: UITableViewController {
                 cell.posterImg.image = image
             }
         }
-        cell.titleLbl.text = films[indexPath.row].titel
-
+        cell.titleLbl.text = filteredFilms[indexPath.row].titel
         return cell
     }
 
@@ -63,14 +81,13 @@ class FilmWatchlistViewController: UITableViewController {
 
 extension FilmWatchlistViewController {
   override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-    currentFilm = films[indexPath.row]
+    currentFilm = filteredFilms[indexPath.row]
     return indexPath
   }
 }
 
 extension FilmWatchlistViewController {
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return films.count
+    return filteredFilms.count
   }
-
 }
